a14e34ffc1620ff6a63818850e635520c98ba4ac
Dylan-Kaneshiro <dylan.kaneshiro14@gmail.com>
2023-11-02 16:19:52 -0500

    added matt_test.py

---
+++ Practice/matt_test.py
@@ -0,0 +1,23 @@
+import gradio as gr
+
+def talk(agent, prompt):
+    return agent.run(prompt)
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            name = gr.Textbox(label="Name")
+            description = gr.Textbox(label="Description")
+            uploaded_file = gr.File()
+            agent = gr.State()
+            create_agent_btn = gr.Button("Create Agent")
+            # use create_agent_btn.click() to apply ur function to name, description, uploaded file, agent as inputs and agent as output
+        with gr.Column():
+            prompt = gr.Textbox(label="prompt")
+            output = gr.Textbox(label="output")
+            prompt_btn = gr.Button("Submit Prompt")
+            prompt_btn.click(talk, 
+                             inputs=[agent, prompt],
+                             outputs=[output])
+
+demo.launch()

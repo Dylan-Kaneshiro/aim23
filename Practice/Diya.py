1a6cb7d48108cfa710bf23f0430be20ff5ac9607
diyatresa <diyatresa.jibu@gmail.com>
2023-11-08 07:57:17 -0600

    New Update

---
+++ Practice/Diya.py
@@ -0,0 +1,25 @@
+import gradio as gr
+
+css_path = "aim23/Practice/stylesheet.css"
+
+
+def chatbot_response(text):
+    return text
+
+examples = [
+    ['Hello'],
+    ['How are you?'],
+    ['What is your name?']
+]
+
+
+    chatbot = gr.Interface(
+        fn=chatbot_response,
+        inputs=gr.Textbox(),
+        outputs=gr.Textbox(),
+        examples=examples,
+        title="CHATBOT",
+        css= "file={css_path}"
+    )
+
+chatbot.launch()

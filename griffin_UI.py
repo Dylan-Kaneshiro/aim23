d0983d5a160b00561705f078863c668ed02fbd45
Dylan-Kaneshiro <dylan.kaneshiro14@gmail.com>
2023-11-08 19:31:05 -0600

    Added Chat History

--- griffin_UI.py
+++ griffin_UI.py
@@ -21,24 +21,36 @@ def respond(message, chat_history, agent):
   bot_message = agent.run(message)
   chat_history.append((message, bot_message))
   time.sleep(2)
-  return "", chat_history
+  return "", chat_history, display_question_and_answer(chat_history)
+
+def display_question_and_answer(tuple_list):
+    qa_history = ""
+    for i, (q, a) in enumerate(tuple_list):
+        qa_history += f"<details><summary>Q: {q}</summary><p>A: {a}</p></details>"
+    return qa_history
 
 with gr.Blocks(css=custom_css) as demo:
-  with gr.Row(variant='compact'):
-    with gr.Column(scale=1, elem_classes='column'):
-      logo = gr.Image(value=logo_filepath, type='filepath', show_label=False, show_download_button=False, container=False, width=75, height=75, elem_classes='column_el')
-      
-      agent = gr.State()
-      file = gr.File(file_types=['pdf'], file_count='single', show_label=False, elem_classes='column_el', height=75)
-      name = gr.Textbox(label="Name", elem_classes='column_el')
-      description = gr.Textbox(label="Description", elem_classes='column_el')
-      create_agent_btn = gr.Button(value='Submit', elem_classes='column_el')
-      create_agent_btn.click(fn=create_agent, inputs = [name, description, file, agent], outputs = agent)
+  with gr.Tab("Home"):  
+    with gr.Row(variant='compact'):
+      with gr.Column(scale=1, elem_classes='column'):
+        logo = gr.Image(value=logo_filepath, type='filepath', show_label=False, show_download_button=False, container=False, width=75, height=175, elem_classes='column_el')
+        
+        agent = gr.State()
+        file = gr.File(file_types=['pdf'], file_count='single', show_label=False, elem_classes='column_el', height=75)
+        name = gr.Textbox(label="Name", elem_classes='column_el')
+        description = gr.Textbox(label="Description", elem_classes='column_el')
+        create_agent_btn = gr.Button(value='Submit', elem_classes='column_el')
+        create_agent_btn.click(fn=create_agent, inputs = [name, description, file, agent], outputs = agent)
 
-      robot = gr.Image(value=robot_filepath, type='filepath', show_label=False, show_download_button=False, container=False, width=125, height=125, elem_classes='column_el')
-    with gr.Column(scale=3, elem_classes='column'):
-      chatbot = gr.Chatbot(elem_classes='column_el')
-      msg = gr.Textbox(show_label=False, elem_classes='column_el')
-      msg.submit(respond, [msg, chatbot, agent], [msg, chatbot])
+        robot = gr.Image(value=robot_filepath, type='filepath', show_label=False, show_download_button=False, container=False, width=125, height=125, elem_classes='column_el')
+      with gr.Column(scale=3, elem_classes='column'):
+        chatbot = gr.Chatbot(elem_classes='column_el')
+        msg = gr.Textbox(show_label=False, elem_classes='column_el')
+        
+  with gr.Tab("Chat History"):
+    history = gr.HTML()
+
+  msg.submit(respond, [msg, chatbot, agent], [msg, chatbot, history])
+    
 
 demo.launch()
